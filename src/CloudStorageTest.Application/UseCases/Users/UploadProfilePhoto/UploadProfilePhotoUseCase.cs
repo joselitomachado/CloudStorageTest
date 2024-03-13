d53116e56f8a2ebaca8c82f4ae051a34263842a2
pague <pague@PAGUELO>
2024-03-13 16:27:45 -0300

    Adicionar arquivos de projeto.

---
+++ src/CloudStorageTest.Application/UseCases/Users/UploadProfilePhoto/UploadProfilePhotoUseCase.cs
@@ -0,0 +1,42 @@
+﻿using CloudStorageTest.Domain.Entities;
+using CloudStorageTest.Domain.Storage;
+using FileTypeChecker.Extensions;
+using FileTypeChecker.Types;
+using Microsoft.AspNetCore.Http;
+
+namespace CloudStorageTest.Application.UseCases.Users.UploadProfilePhoto;
+public class UploadProfilePhotoUseCase : IUploadProfilePhotoUseCase
+{
+    private readonly IStorageService _storageService;
+
+    public UploadProfilePhotoUseCase(IStorageService storageService)
+    {
+        _storageService = storageService;
+    }
+
+    public void Execute(IFormFile file)
+    {
+        var fileStream = file.OpenReadStream();
+
+        var isImage = fileStream.Is<JointPhotographicExpertsGroup>();
+
+        if (isImage == false)
+            throw new Exception("O arquivo não é uma imagem.");
+
+        var user = GetFromDatabase();
+
+        _storageService.Upload(file, user);
+    }
+
+    private User GetFromDatabase() 
+    {
+        return new User
+        {
+            Id = 1,
+            Name = "",
+            Email = "",
+            RefreshToken = "",
+            AcessToken = ""
+        };
+    }
+}
